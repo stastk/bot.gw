400db0a5d830199f13346f9c54c2f44c485f200d
Stas <me@stas.tk>
2023-10-07 01:27:58 +0300

    router added

--- main.go
+++ main.go
@@ -1,14 +1,34 @@
 package main
 
 import (
+	"io"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
+type Response struct {
+	Status     string // e.g. "200 OK"
+	StatusCode int    // e.g. 200
+	Proto      string // e.g. "HTTP/1.0"
+	ProtoMajor int    // e.g. 1
+	ProtoMinor int    // e.g. 0
+
+	// response headers
+	Header http.Header
+	// response body
+	Body io.ReadCloser
+	// request that was sent to obtain the response
+	Request *http.Request
+}
+
+func setupRouter() *gin.Engine {
 	r := gin.Default()
 
+	// Ping test
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"ANSWER": "PONG"})
+	})
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"data": "hello world "})
 	})
@@ -18,6 +38,17 @@ func main() {
 	r.POST("/tg", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"data": "hello world "})
 	})
+	r.POST("/tg/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": "hello world "})
+	})
 
+	return r
+
+}
+
+func main() {
+
+	r := setupRouter()
+	// Listen and Server in 0.0.0.0:9981
 	r.Run(":9981")
 }
