1ea9e12b5ce63ab90e005ca728cb1d48be33c329
Stas <me@stas.tk>
2023-10-06 23:18:24 +0300

    init

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": "hello world "})
+	})
+	r.POST("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": "hello world "})
+	})
+
+	r.Run(":9990")
+}
